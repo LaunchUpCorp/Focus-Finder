67f32f45ef064c8e9816f02e16bb67b2a42c59b1
mjx <mattheweoro@gmail.com>
2023-04-26 20:41:14 -0400

    feat(extension): Install and setup graphql client

--- focus-finder-chrome-extension/src/main.tsx
+++ focus-finder-chrome-extension/src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ApolloProvider } from '@apollo/client'
+import client from './GraphQL/index.ts'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ApolloProvider client={client} >
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+    </ApolloProvider>
 )
