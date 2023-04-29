f8d2ab3beaa1eaa8f94b95b1fcdc6621881a152a
MjxOro <mattheweoro@gmail.com>
2023-04-29 16:02:55 -0400

    refactor(extension): update state data structure to handle errors on auth input.

--- focus-finder-chrome-extension/src/pages/AuthPage.tsx
+++ focus-finder-chrome-extension/src/pages/AuthPage.tsx
@@ -5,34 +5,59 @@ import Input from '../components/Input';
 import Button from '../components/Button';
 
 const AuthPage: FC = () => {
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const [addUser] = useMutation(ADD_USER);
+  const [email, setEmail] = useState<{message: string, error: boolean}>({message: '', error: false});
+  const [password, setPassword] = useState<{message: string, error: boolean}>({message: '', error: false});
+  const [addUser, {data}] = useMutation(ADD_USER);
   const handleChangeEmail = (e: ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
+    setEmail({'message': e.target.value, error: false});
   };
   const handleChangePassword = (e: ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
+    setPassword({'message': e.target.value, error: false});
   };
   const handleSubmit = () => {
-    // POST AUTH API USING GRAPHQL
-    addUser({
-      variables: { email: email, password: password }
-    });
-    setEmail('');
-    setPassword('');
+    try {
+      // Validate Input
+      if(!email.message || !password.message){
+        if(!email.message){
+          // handle no input email
+          setEmail((prev) => ({...prev, error: true}));
+        }
+        if(!password.message){
+          // handle no input password
+          setPassword((prev) => ({...prev, error: true}));
+        }
+        return
+      }
+      // POST AUTH API USING GRAPHQL
+
+      addUser({
+        variables: { email: email, password: password }
+      });
+
+      setEmail((prev) => ({...prev, message: ''}));
+      setPassword((prev) => ({...prev, message: ''}));
+
+    }catch(e) {
+      console.error(e)
+
+    }
   };
   return (
-    <div>
-      <h1>Login</h1>
-      <Input message="Email" value={email} onChange={handleChangeEmail} />
-      <Input
-        message="Password"
-        value={password}
-        type="password"
-        onChange={handleChangePassword}
-      />
-      <Button message="submit" onClick={handleSubmit} />
+    <div className='w-full h-full'>
+      <h1 className='w-full text-center my-4'>Focus Finder</h1>
+      <div className='flex flex-col justify-center items-center '>
+        <h2 className='text-2xl'>Login</h2>
+        <Input message="Email" value={email.message} error={email.error} onChange={handleChangeEmail} />
+        <Input
+          message="Password"
+          value={password.message}
+          error={password.error}
+          type="password"
+          onChange={handleChangePassword}
+        />
+        <Button message="submit" onClick={handleSubmit} />
+        <a className='cursor-pointer text-lg' onClick={() => window.open(import.meta.env.VITE_APP_WEBSITE_URL)}>Don't have an account?</a>
+      </div>
     </div>
   );
 };
