0e1f022b5e3124f843a65e706e2baca6ca5b040f
MjxOro <mattheweoro@gmail.com>
2023-04-29 16:04:03 -0400

    refactor(extension): import auth page to app.tsx

--- focus-finder-chrome-extension/src/App.tsx
+++ focus-finder-chrome-extension/src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
+import AuthPage from './pages/AuthPage';
 
 function App() {
   return (
-    <div className='w-screen min-w-full h-80 min-h-full text-2xl text-white flex justify-center items-center'>
-      <p>Hello World</p>
+    <div className='w-screen min-w-full h-80 min-h-full justify-center items-center'>
+      <AuthPage />
     </div>
   );
 }
