0fdb168d6f378f0f2bc14df4db347545fe7bab0d
MjxOro <mattheweoro@gmail.com>
2023-05-02 12:41:15 -0400

    feat(server): add JwtService module to auth

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  providers: [AuthResolver, AuthService]
+  providers: [AuthResolver, AuthService, JwtService]
 })
 export class AuthModule {}
