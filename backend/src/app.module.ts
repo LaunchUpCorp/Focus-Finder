3a78586984e5dcab1e1358066a9bcdd437b6ba6d
MjxOro <mattheweoro@gmail.com>
2023-05-02 12:44:52 -0400

    refactor(server): add ConfigModule to app

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,11 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
