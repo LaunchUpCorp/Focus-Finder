a2b1ae392a40cb64d89a2adb9131c4d30c753e99
Hyunchol Jun <hyunchol.jun@gmail.com>
2023-05-03 15:57:26 -0400

    feat: set up GraphQL with Apollo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,27 @@
 import "./App.css";
+import { useQuery, gql } from "@apollo/client";
+
+const QUERY = gql`
+  query {
+    user(id: 1) {
+      id
+      email
+    }
+  }
+`;
 
 function App() {
+  const { loading, error, data } = useQuery(QUERY);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world</h1>
+      <p>
+        User ID: {data.user.id}, EMAIL: {data.user.email}
+      </p>
     </>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const client = new ApolloClient({
+  uri: import.meta.env.VITE_SERVER_URL,
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);
