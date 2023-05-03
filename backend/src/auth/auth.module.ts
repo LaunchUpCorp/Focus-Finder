9dc2a81b3e05a8a7685261759292a6d9605df24d
MjxOro <mattheweoro@gmail.com>
2023-05-03 12:19:51 -0400

    feat(server): import auth services and strategies

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,8 +3,17 @@ import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma.service';
+import { AccessTokenStrategy } from './strategies/accessToken.strategy';
+import { RefreshTokenStrategy } from './strategies/refreshToken.strategy';
 
 @Module({
-  providers: [AuthResolver, AuthService, JwtService, PrismaService]
+  providers: [
+    AuthResolver,
+    AuthService,
+    JwtService,
+    PrismaService,
+    AccessTokenStrategy,
+    RefreshTokenStrategy
+  ],
 })
 export class AuthModule {}
